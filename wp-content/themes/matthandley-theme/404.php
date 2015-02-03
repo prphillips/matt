cafefee4a6fcb488f1cc34a00943f2cb2e0d1df7
prphillips <petephillips777@gmail.com>
2015-02-03 23:49:52 +0000

    404 logo tweaks

--- wp-content/themes/matthandley-theme/404.php
+++ wp-content/themes/matthandley-theme/404.php
@@ -2,7 +2,7 @@
 
 				<div class="row">
 					<div class="container">
-						<h1 class="tagline" style="margin-top:42vh">Uh-oh! Page not found.</h1>
+						<h1 class="tagline" style="margin-top:30vh">Uh-oh! Page not found.</h1>
 					</div>
 				</div>
 			</div>
