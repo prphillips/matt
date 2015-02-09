5322c85a74c3860d3c74c89a1dbbf8efdec10100
petephillips777@gmail.com <petephillips777@gmail.com>
2015-02-09 23:00:00 +0000

    correcting db name

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'uks77111matt');
+define('DB_NAME', 'uks77111matth');
 
 /** MySQL database username */
 define('DB_USER', 'uks77111mattuser');
